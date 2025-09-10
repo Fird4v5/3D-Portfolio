63293feaa43c780c2e872d105d67d1a93fb41aa6
Firdavs Yuldashev <firdavsyuldashev111@gmai.com>
2025-09-10 16:29:40 +0500

    bug fixes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,8 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import { EarthCanvas, ComputersCanvas, StarsCanvas } from './canvas';
 import Hero from './Hero';
 import Navbar from './Navbar'; 
 import About from './About'; 
-import Tech from './Tech';
-import Experience from './Experience'; 
-import Projects from './Projects';
-import Feedbacks from './Feedbacks'; 
+import Projects from './Projects'; 
 import Contact from './Contact'; 
 import ProjectCard from './ProjectCard';
 
@@ -13,13 +10,9 @@ export {
   Hero,
   Navbar,
   About,
-  Tech,
-  Experience,
   Projects,
-  Feedbacks,
   Contact,
   EarthCanvas, 
-  BallCanvas, 
   ComputersCanvas, 
   StarsCanvas, 
   ProjectCard
