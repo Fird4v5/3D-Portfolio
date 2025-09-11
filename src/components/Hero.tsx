1f2e932a88e0495070dfe57d0637a7a96f6904dc
Firdavs Yuldashev <firdavsyuldashev111@gmai.com>
2025-09-11 16:18:12 +0500

    minor change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Firdavs</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop web apps, <br className='sm:block hidden' />3D visuals, telegram bots and miniapps
+            I develop web apps, <br className='sm:block hidden' />telegram bots and miniapps
           </p>
         </div>
       </div>
